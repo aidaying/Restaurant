66d95fbc6657c6478bdc613c6afcc798a6c58849
Tsz Ying Chung <aida_ying@hotmail.com>
2017-11-10 14:38:40 +1300

    remove docker and add a

---
+++ Restaurant/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public String Username { get; set; }
+        public int Mobile { get; set; }
+        public String Email { get; set; }
+        public String Street { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public int PostCode { get; set; }
+    }
+}
